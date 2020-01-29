773077d39fc88561d3251cf4994105debe49bfe3
steekam <waynewanyee@gmail.com>
2020-01-29 13:48:56 +0300

    Project init

---
+++ coupons-cart.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name:       Coupons Cart
+ * Description:       Integrates payment options onto a form
+ * Version:           1.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.3
+ * Author:            Slashdot Labs Limited
+ * Author URI:        https://slashdotlabs.com/
+ */
+
+// Prevent direct access
+if( !defined('ABSPATH')) {
+    echo "We don't do that here :/";
+    exit;
+}
