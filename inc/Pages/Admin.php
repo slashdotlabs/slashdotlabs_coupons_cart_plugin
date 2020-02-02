c570038f1de7d006f055db3792a842cd6e3a0987
Allan Vikiru <vikiruallan12@gmail.com>
2020-02-02 21:23:57 +0300

    added: complete frontend admin config settings

--- inc/Pages/Admin.php
+++ inc/Pages/Admin.php
@@ -40,6 +40,118 @@ class Admin extends BaseController {
             ]
         );
     }
+
+    
+    public function setSettings(){
+        //coupon settings array
+        $coupon_settings = array(
+            [
+                'option_group' => 'ccart_admin_group',
+                'option_name' => 'vendor_id',
+                'callback' => array( $this->callbacks, 'ccartAdminGroup'),
+                
+            ],
+            [
+                'option_group' => 'ccart_admin_group',
+                'option_name' => 'hashkey',
+                'callback' => array( $this->callbacks, 'ccartAdminGroup'),
+                
+            ],
+            [
+                'option_group' => 'ccart_admin_group',
+                'option_name' => 'address_from_name',
+                'callback' => array( $this->callbacks, 'ccartAdminGroup'),
+                
+            ],
+            [
+                'option_group' => 'ccart_admin_group',
+                'option_name' => 'address_from_email',
+                'callback' => array( $this->callbacks, 'ccartAdminGroup'),
+                
+            ],
+
+        );
+        $this->settings->setSettings($coupon_settings);
+    }
+    public function setSections(){
+        $args = array(
+            [
+                'id' => 'ccart_ipay_index',
+                'title' => 'iPay Settings',
+                'callback' => array( $this->callbacks, 'ccartIpaySection'),
+                'page' => 'coupons_plugin'
+                
+            ],
+            [
+                'id' => 'ccart_mail_index',
+                'title' => 'Mailing Settings',
+                'callback' => array( $this->callbacks, 'ccartMailSection'),
+                'page' => 'coupons_plugin'
+                
+            ],
+
+        );
+        $this->settings->setSections( $args );
+
+    }
+    public function setFields(){
+        $args = array(
+            [
+                'id' => 'vendor_id',
+                'title' => 'Vendor ID',
+                'callback' => array( $this->callbacks, 'ccartVendorId'),
+                'page' => 'coupons_plugin',
+                'section' => 'ccart_ipay_index',
+                'args' => array(
+                    'label_for' => 'vendor_id',
+                    'class' => 'example-class',
+
+                )
+                
+            ],
+            [
+                'id' => 'hashkey',
+                'title' => 'Hash Key',
+                'callback' => array( $this->callbacks, 'ccartHashkey'),
+                'page' => 'coupons_plugin',
+                'section' => 'ccart_ipay_index',
+                'args' => array(
+                    'label_for' => 'hashkey',
+                    'class' => 'example-class',
+
+                )
+                
+            ],
+            [
+                'id' => 'address_from_name',
+                'title' => 'Sender Username',
+                'callback' => array( $this->callbacks, 'ccartAddressFromName'),
+                'page' => 'coupons_plugin',
+                'section' => 'ccart_mail_index',
+                'args' => array(
+                    'label_for' => 'address_from_name',
+                    'class' => 'example-class',
+
+                )
+                
+            ],
+            [
+                'id' => 'address_from_email',
+                'title' => 'Sender Email Address',
+                'callback' => array( $this->callbacks, 'ccartAddressFromEmail'),
+                'page' => 'coupons_plugin',
+                'section' => 'ccart_mail_index',
+                'args' => array(
+                    'label_for' => 'address_from_email',
+                    'class' => 'example-class',
+
+                )
+                
+            ],
+        );
+        $this->settings->setFields( $args );
+    }
+
     public function register(){
 
         $this->settings = new SettingsApi();
@@ -47,6 +159,10 @@ class Admin extends BaseController {
 
         $this->setPages();
         $this->setSubPages();
+
+        $this->setSettings();
+        $this->setSections();
+        $this->setFields();
 
         $this->settings->addPages( $this->pages )
                         ->withSubPage('Configuration Settings')
